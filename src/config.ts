ef7d5a3542458410cda1ee33852cc8f6ceedd398
xsxinghen <171335350@qq.com>
2025-03-25 19:02:43 +0800

    update

--- src/config.ts
+++ src/config.ts
@@ -51,7 +51,7 @@ export const navBarConfig: NavBarConfig = {
     //  name: 'GitHub',
     //  url: 'https://github.com/saicaca/fuwari',     // Internal links should not include the base path, as it is automatically added
     //  external: true,                               // Show an external link icon and will open in a new tab
-   // },
+    // },
   ],
 }
 
