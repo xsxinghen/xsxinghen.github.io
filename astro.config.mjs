fbe1f8d351b1d94c5ed2be58e3bb7ac8123f22e9
xsxinghen <171335350@qq.com>
2025-03-26 01:02:56 +0800

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -21,7 +21,7 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://blog.313579.xyz/",
+  site: "https://xsxinghen.github.io/",
   base: "/",
   trailingSlash: "always",
   integrations: [
