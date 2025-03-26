a9d519a3c3cb94ce57a9e84760c09aa0324b30da
xsxinghen <171335350@qq.com>
2025-03-26 21:21:05 +0800

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -18,7 +18,6 @@ import { GithubCardComponent } from "./src/plugins/rehype-component-github-card.
 import { parseDirectiveNode } from "./src/plugins/remark-directive-rehype.js";
 import { remarkExcerpt } from "./src/plugins/remark-excerpt.js";
 import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
-import yaml from "@rollup/plugin-yaml";
 
 // https://astro.build/config
 export default defineConfig({
@@ -127,7 +126,6 @@ export default defineConfig({
           }
           warn(warning);
         },
-        plugins: [yaml()],
       },
     },
     
