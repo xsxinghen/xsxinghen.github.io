5c1388e2351319d5bcbed7cede9aab94e88e3e7b
xsxinghen <84395857+xsxinghen@users.noreply.github.com>
2025-10-09 00:37:27 +0800

    Update zh_TW.ts via web editor

--- src/i18n/languages/zh_TW.ts
+++ src/i18n/languages/zh_TW.ts
@@ -6,7 +6,7 @@ export const zh_TW: Translation = {
 	[Key.about]: "關於",
 	[Key.archive]: "彙整",
 	[Key.search]: "搜尋",
-
+        [Key.donate]: "捐贈",
 	[Key.tags]: "標籤",
 	[Key.categories]: "分類",
 	[Key.recentPosts]: "最新文章",
