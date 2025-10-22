babb3e861cf1b1c3b0597cbf2c0eb45a83d5e0e5
xsxinghen <84395857+xsxinghen@users.noreply.github.com>
2025-10-22 20:14:39 +0800

    Update content-utils.ts via web editor

--- src/utils/content-utils.ts
+++ src/utils/content-utils.ts
@@ -1,6 +1,4 @@
 import { type CollectionEntry, getCollection } from "astro:content";
-import I18nKey from "@i18n/i18nKey";
-import { i18n } from "@i18n/translation";
 import { getCategoryUrl } from "@utils/url-utils.ts";
 
 // // Retrieve posts and sort them by publication date
