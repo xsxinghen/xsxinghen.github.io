ef657378e7ddbe971d12c25149485c1ec22506bf
xsxinghen <84395857+xsxinghen@users.noreply.github.com>
2025-10-22 20:13:51 +0800

    Update url-utils.ts via web editor

--- src/utils/url-utils.ts
+++ src/utils/url-utils.ts
@@ -1,5 +1,3 @@
-import I18nKey from "@i18n/i18nKey";
-import { i18n } from "@i18n/translation";
 
 export function pathsEqual(path1: string, path2: string) {
 	const normalizedPath1 = path1.replace(/^\/|\/$/g, "").toLowerCase();
